f7aec8023d9f4f86bace8f21948debc43d9db6a0
JerrodMcHenry <joinjerrodtoday@gmail.com>
2026-04-08 18:58:39 -0700

    Added web search ability to the chatbot

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uuid
 import os
 from datetime import datetime
 from dotenv import load_dotenv
@@ -5,7 +6,12 @@ from dotenv import load_dotenv
 from langchain.agents import create_agent # Requires langchain >= 0.3.0
 from langchain_core.tools import tool
 from langchain_ollama import ChatOllama
+import gradio as gr
 
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
+
+from langchain_community.tools.tavily_search import TavilySearchResults
 
 # 1. Load environment variables
 load_dotenv()
@@ -17,6 +23,10 @@ def get_date():
     """Returns the current date in YYYY-MM-DD format."""
     return datetime.now().strftime("%Y-%m-%d")
 
+search_tool = TavilySearchResults()
+conn = sqlite3.connect("chatbot_memory.db", check_same_thread=False)
+checkpointer = SqliteSaver(conn)
+
 # 3. Initialize the Gemini model
 # Gemini 3.0+ models require a temperature of 1.0 or higher
 llm = ChatOllama(
@@ -26,12 +36,31 @@ llm = ChatOllama(
 )
 
 # 4. Create the agent
-system_prompt = "You are a helpful assistant."
-agent = create_agent(model=llm, tools=[get_date], system_prompt=system_prompt)
+system_prompt = "You are a helpful assistant. Answer all users queries."
+agent = create_agent(
+                     model=llm, 
+                     tools=[get_date, search_tool],
+                     system_prompt=system_prompt, 
+                     checkpointer=checkpointer)
 
 # 5. Execute the agent (not the llm directly)
-user_query = input("Enter a query (e.g., 'What is today's date?'): ")
-response = agent.invoke({"messages": [{"role": "user", "content": user_query}]})
+# user_query = input("Enter a query (e.g., 'What is today's date?'): ")
+
 
 # 6. Display the final response
-print(response['messages'][-1].content)
+# print(response['messages'][-1].content)
+
+def chat(message, history, thread_id):
+    config = {"configurable": {"thread_id": thread_id}}
+    response = agent.invoke({"messages": [{"role": "user", "content": message}]},
+                        config
+    )
+    last_response = response['messages'][-1].content
+    return last_response
+
+with gr.Blocks() as demo:
+    gr.Markdown("# Ai Chatbot")
+    thread_id = gr.State(value=lambda: str(uuid.uuid4()))
+    gr.ChatInterface(fn=chat, additional_inputs=thread_id)
+
+demo.launch()
