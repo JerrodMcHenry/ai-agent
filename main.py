67ea668c3711171ae5d8ca9a010a10454d84a1a0
JerrodMcHenry <joinjerrodtoday@gmail.com>
2026-04-08 18:16:36 -0700

    Implemented local ai agent

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 from datetime import datetime
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent # Requires langchain >= 0.3.0
 from langchain_core.tools import tool
+from langchain_ollama import ChatOllama
+
 
 # 1. Load environment variables
 load_dotenv()
-api_key = os.getenv("GOOGLE_API_KEY")
+# api_key = os.getenv("GOOGLE_API_KEY")
 
 # 2. Define the tool with a decorator for proper metadata
 @tool
@@ -17,9 +19,9 @@ def get_date():
 
 # 3. Initialize the Gemini model
 # Gemini 3.0+ models require a temperature of 1.0 or higher
-llm = ChatGoogleGenerativeAI(
-    model="gemini-3-flash-preview", 
-    google_api_key=api_key,
+llm = ChatOllama(
+    model="qwen3.5:4b", 
+    # google_api_key=api_key,
     temperature=1.0 
 )
 
@@ -32,4 +34,4 @@ user_query = input("Enter a query (e.g., 'What is today's date?'): ")
 response = agent.invoke({"messages": [{"role": "user", "content": user_query}]})
 
 # 6. Display the final response
-print(response["messages"][-1].content)
+print(response['messages'][-1].content)
